8977bcc35cc762e7a179626bfceb753f5a182010
sntddm <sntddm77@gmail.com>
2026-06-30 19:15:15 +0200

    pgvector

--- src/main/java/sa/logiceval/catalog/internal/VectorStoreConfig.java
+++ src/main/java/sa/logiceval/catalog/internal/VectorStoreConfig.java
@@ -7,17 +7,26 @@ import org.springframework.ai.vectorstore.SimpleVectorStore;
 // import org.springframework.ai.openai.api.OpenAiApi;
 // import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
-
+// import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 class VectorStoreConfig {
 
     // For local development, an in-memory SimpleVectorStore works perfectly
+    // @Bean
+    // public VectorStore vectorStore(EmbeddingModel embeddingModel) {
+    // // return new SimpleVectorStore(embeddingModel);
+    // return SimpleVectorStore.builder(embeddingModel).build();
+    // }
+
     @Bean
-    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
-        // return new SimpleVectorStore(embeddingModel);
-        return SimpleVectorStore.builder(embeddingModel).build();
+    public VectorStore ollamaVectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel embeddingModel) {
+        // Creates a real connection utilizing Postgres's native vector operations
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel).build();
     }
 }
